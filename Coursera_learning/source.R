b90a9dc88f3e84fe0f86b98d7ca82e6e0c51545e
Trishna Saha Detroja <61427628+trishnasaha@users.noreply.github.com>
2023-07-09 17:39:39 -0400

    added

--- Coursera_learning/source.R
+++ Coursera_learning/source.R
@@ -68,4 +68,23 @@ myMatrix$getsolve()
 
 cacheSolve(myMatrix)
 
+source('best.R')
+best("SC", "heart attack")
+best("NY", "pneumonia")
+best("AK", "pneumonia")
 
+source("rankhospital.R")
+rankhospital("NC", "heart attack", "worst")
+rankhospital("WA", "heart attack", 7)
+rankhospital("NY", "heart attack", 7)
+rankhospital("TX", "pneumonia", 10)
+
+source("rankall.R")
+r <- rankall("heart attack", 4)
+as.character(subset(r, state == "HI")$hospital)
+
+r <- rankall("pneumonia", "worst")
+as.character(subset(r, state == "NJ")$hospital)
+
+r <- rankall("heart failure", 10)
+as.character(subset(r, state == "NV")$hospital)
